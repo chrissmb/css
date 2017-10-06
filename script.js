988b83c0e68477b859e05c84ccfeaa0f1b5bba14
Christopher <chrissantista@gmail.com>
2017-10-05 22:22:10 -0300

    Melhorado menu dropdown sidebar

--- script.js
+++ script.js
@@ -1,21 +1,32 @@
+// Code goes here
+
 $(document).ready(function(){
-    var dropdownAtivo = false;
-
+    
     $(document).click(function() {
-        $(".dropdown-content").hide();
+      $(".dropdown-content").removeClass("show");
     });
-
-    $(".dropdown").click(function(){
-        $(".dropdown-content").not(this)
-            .not($(this).children(".dropdown-content")).hide();
-        $(this).children(".dropdown-content").toggle();
+    
+    $(".dropdown").click(function() {    
+      $(".dropdown-content").not(this)
+          .not($(this).children(".dropdown-content")).removeClass("show");
+      $(this).children(".dropdown-content").toggleClass("show");
+      event.stopPropagation();
+    });
+    
+    $(".dropdown-content").click(function() {
+      var eMobile = $(window).width() < 600;
+      var eSidebar = $(this).parents(".sidebar").length;
+      
+      if (eMobile || eSidebar) {
         event.stopPropagation();
+      }
     });
-
+    
     $(".dropdown").children("a").each( function() {
-        $(this).html(
-        $(this).html() + " <span style='font-size:9px'>&#9660;</span>"
-        );
+      $(this).html(
+        $(this).html() + " <span class='caret'>&#9660;</span>"
+      );
     });
-
-});
+    
+  });
+  
