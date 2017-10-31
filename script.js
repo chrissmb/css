875a8cfcc591dbd4a18e59f47106be50fe2abe72
Christopher <chrissantista@gmail.com>
2017-10-30 22:55:44 -0200

    Ajustes de modal.

--- script.js
+++ script.js
@@ -1,32 +1,59 @@
 // Code goes here
 
 $(document).ready(function(){
-    
-    $(document).click(function() {
-      $(".dropdown-content").removeClass("show");
-    });
-    
-    $(".dropdown").click(function() {    
-      $(".dropdown-content").not(this)
-          .not($(this).children(".dropdown-content")).removeClass("show");
-      $(this).children(".dropdown-content").toggleClass("show");
-      event.stopPropagation();
-    });
-    
-    $(".dropdown-content").click(function() {
-      var eMobile = $(window).width() < 600;
-      var eSidebar = $(this).parents(".sidebar").length;
-      
-      if (eMobile || eSidebar) {
-        event.stopPropagation();
-      }
-    });
-    
-    $(".dropdown").children("a").each( function() {
-      $(this).html(
-        $(this).html() + " <span class='caret'>&#9660;</span>"
-      );
-    });
-    
+
+  $(document).click(function() {
+    $(".dropdown-content").removeClass("show");
   });
-  
+
+  $(".dropdown").click(function() {
+    $(".dropdown-content").not(this)
+        .not($(this).children(".dropdown-content")).removeClass("show");
+    $(this).children(".dropdown-content").toggleClass("show");
+    event.stopPropagation();
+  });
+
+  $(".dropdown-content").click(function() {
+    var eMobile = $(window).width() < 600;
+    var eSidebar = $(this).parents(".sidebar").length;
+
+    if (eMobile || eSidebar) {
+      event.stopPropagation();
+    }
+  });
+
+  $(".dropdown").children("a").each( function() {
+    $(this).append(" <span class='caret'>&#9660;</span>");
+  });
+
+  var temModal = $(".modal").length;
+  if(temModal) {
+    $("body").append('<div class="modal-bg-black"></div>');
+  }
+
+  var iconeFechar = '<a href="javascript:void(0)" class="fecha-modal">&times;</a>';
+  if (temModal) {
+    $(".modal").prepend(iconeFechar);
+  }
+
+  $("[modal]").click(function() {
+    var scrollPosition = $(window).scrollTop();
+    var modal = $(this).attr("modal");
+    var idModal = "#" + modal;
+    $(idModal).addClass("show");
+    $(".modal-bg-black").addClass("show");
+    $("body").addClass("modal-aberto");
+    $("modal-aberto").css("top", -(scrollPosition));
+   });
+
+  $(".modal-bg-black,.fecha-modal").click(function() {
+    fechaModal();
+  });
+
+});
+
+function fechaModal(){
+  $(".modal-bg-black").removeClass("show");
+  $(".modal").removeClass("show");
+  $("body").removeClass("modal-aberto");
+}
